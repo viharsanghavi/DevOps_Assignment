cab433d32048f8fe59311ab1c911c26ebe3db7dc
Vihar Sanghavi <vihar@promact.com>
2017-02-23 16:57:11 +0530

    my changes

--- bin/travis.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"D:\DevOps\DevOps_Assignment\bin\ruby.exe" "D:/DevOps/DevOps_Assignment/bin/travis" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"D:\DevOps\DevOps_Assignment\bin\ruby.exe" "%~dpn0" %*
